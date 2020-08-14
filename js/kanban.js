c6577463a6008e18f0856a6b788d747ba0f5d6ae
James James <jamesjay4199@gmail.com>
2020-08-14 23:20:17 +0100

    complete project

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,50 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.id = 'item-' + order;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', (event) => {
+    return event.dataTransfer.setData('text', event.target.id);
+  });
+
+  item.addEventListener('dragend', (event) => {
+    return event.dataTransfer.clearData();
+  });
+
+  const input = document.createElement('input');
+  item.appendChild(input);
+
+  const save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+
+  save_btn.addEventListener('click', (event) => {
+    error.innerHTML = '';
+
+    if (input.value !== '') {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  });
+  item.appendChild(save_btn);
+
+  return item;
 };
 
-document.querySelectorAll('.drop').forEach(element => {
+document.querySelectorAll('.drop').forEach((element) => {
+  element.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    const item = document.getElementById(id);
+    `event.target`.appendChild(document.getElementById(id));
+  });
+
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
 });
